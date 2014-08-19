c586d07a70d8af9db9b15dd3a9e75d1a94df3802
nvg58 <nvangiap5894@gmail.com>
2014-08-19 23:19:44 +0700

    refactor functions.php

--- functions.php
+++ functions.php
@@ -87,7 +87,6 @@ require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/shortcode.php';
 
 
-
 /**
  * Widgets for this theme
  */
@@ -347,7 +346,9 @@ function create_case_study() {
 add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
 
 function add_my_post_types_to_query( $query ) {
-	if ( is_home() && $query->is_main_query() )
+	if ( is_home() && $query->is_main_query() ) {
 		$query->set( 'post_type', array( 'post', 'careers', 'case_study' ) );
+	}
+
 	return $query;
 }
